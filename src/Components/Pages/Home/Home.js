884d3983a90083b13df9bf01ab9e5cf03f88854d
hossain101199 <hossainbhai1011@gmail.com>
2022-05-08 18:50:09 +0600

    api done

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Homebanner from "./Homebanner/Homebanner";
 import Homeproducts from "./Homeproducts";
 
 const Home = () => {
   return (
     <div className="container">
+      <Homebanner></Homebanner>
       <Homeproducts></Homeproducts>
     </div>
   );
